bd1614aef4f4f00ad3bd02ce0f2f6e1344db9b3e
junichi7381 <skyhigh8111@gmail.com>
2019-11-23 14:02:30 +0900

    create_incremental-search

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,24 +1,45 @@
-$(function(){
-  
-  var html = `<div class="message">
-                    <div class="upper-message">
-                      <div class="upper-message__user-name">
-                      ${message.user_name}
-                      </div>
-                      <div class="upper-message__date">
-                      ${message.created_at}
-                      </div>
-                    </div>
-                    <div class="lower-message">
-                      <p class="lower-message__content">
-                      ${message.content}
-                      </p>
-                      ${imagehtml}
-                    </div>
-                  </div> `
-    return html;
-  
-  
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+       </div>`
+     return html;
+   } else {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="upper-message">
+           <div class="upper-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="upper-message__date">
+             ${message.date}
+           </div>
+         </div>
+         <div class="lower-message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+       </div>`
+     return html;
+   };
+ }
+ 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -33,7 +54,7 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
       $('#message_content').reset();
       $('.form__submit').prop('disabled', false);
     })
